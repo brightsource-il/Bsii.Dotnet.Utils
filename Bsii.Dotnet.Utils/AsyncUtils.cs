93548fde0b1aed923c549ef0982ef6bfc3b9c56b
Eitan Har-Shoshanim <eitanhs@bseinc.com>
2019-07-08 11:45:35 +0300

    initial commit

---
+++ Bsii.Dotnet.Utils/AsyncUtils.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Bsii.Dotnet.Utils
+{
+    public static class AsyncUtils
+    {
+        public static async Task<List<TOut>> ParallelTransform<TIn, TOut>
+        (
+            IEnumerable<TIn> data,
+            Func<TIn, Task<TOut>> transform,
+            int maxDegreeOfParallelism,
+            string description,
+            Action<Exception> exceptionLogger = null
+        )
+        {
+            async Task<TOut> WrappedTransform(TIn i)
+            {
+                try
+                {
+                    return await transform(i);
+                }
+                catch (Exception ex)
+                {
+                    exceptionLogger?.Invoke(ex);
+                    return default(TOut);
+                }
+            }
+
+            var tb = new TransformBlock<TIn, TOut>(
+                (Func<TIn, Task<TOut>>) WrappedTransform,
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = maxDegreeOfParallelism
+                });
+
+            int nPosted = 0;
+            foreach (var d in data)
+            {
+                if (tb.Post(d))
+                {
+                    nPosted++;
+                }
+            }
+            tb.Complete();
+
+            var res = new List<TOut>();
+            while (nPosted-- > 0)
+            {
+                var r = await tb.ReceiveAsync();
+                if (r != null)
+                {
+                    res.Add(r);
+                }
+
+            }
+
+            await tb.Completion;
+            return res;
+        }
+        
+    }
+}
---
+++ Bsii.Dotnet.Utils/PathUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Bsii.Dotnet.Utils
+{
+    public static class PathUtils
+    {
+        /// <summary>
+        /// Try to find one of the given file names in one of the paths defined in the PATH environment variable
+        /// (e.g. pass "git" & "git.exe" as fileNames to do cross-platform search of the GIT executable)
+        /// </summary>
+        /// <param name="fileNames">Names of files to look for</param>
+        /// <returns>The first match of a file name within one of the paths defined within the PATH env. variable</returns>
+        public static string TryFindInPath(params string[] fileNames)
+        {
+            var pathEnvVar = Environment.GetEnvironmentVariable("PATH");
+            if (pathEnvVar == null)
+            {
+                return null;
+            }
+
+            var values = pathEnvVar.Split(Path.PathSeparator);
+            foreach (var fileName in fileNames)
+            {
+                if (File.Exists(fileName))
+                {
+                    return Path.GetFullPath(fileName);
+                }
+
+                foreach (var path in values)
+                {
+                    var fullPath = Path.Combine(path, fileName);
+                    if (File.Exists(fullPath))
+                    {
+                        return fullPath;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
