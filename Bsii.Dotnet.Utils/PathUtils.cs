cc6a573e7c7bd1957d7a7e248537dda43e597248
Eitan Har-Shoshanim <eitanhs@bseinc.com>
2019-08-13 16:31:50 +0300

    added PathUtils.MakeRelativePath

---
+++ Bsii.Dotnet.Utils/PathUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Bsii.Dotnet.Utils
+{
+    public static class PathUtils
+    {
+        public static string MakeRelativePath(string baseDir, string filePath)
+        {
+            if (string.IsNullOrEmpty(baseDir))
+            {
+                throw new ArgumentNullException(nameof(baseDir));
+            }
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            if (!baseDir.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                baseDir = baseDir + Path.DirectorySeparatorChar;
+            }
+
+            var fromUri = new Uri(baseDir);
+            var toUri = new Uri(filePath);
+
+            if (fromUri.Scheme != toUri.Scheme)
+            {
+                return filePath;
+            } // path can't be made relative.
+
+            var relativeUri = fromUri.MakeRelativeUri(toUri);
+            var relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (toUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase))
+            {
+                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            }
+
+            return relativePath;
+        }
+    }
+}
