7fbf881bcf9f74dcd8496adfd71992310ecd5edb
Gregory Ravikovich <gravikovich@BrightSourceEnergy.com>
2020-05-07 16:20:44 +0300

    Minor documentation

--- Bsii.Dotnet.Utils/Sequential/SequentialOperationsDispatcherExtensions.cs
+++ Bsii.Dotnet.Utils/Sequential/SequentialOperationsDispatcherExtensions.cs
@@ -5,6 +5,14 @@ namespace Bsii.Dotnet.Utils.Sequential
 {
     public static class SequentialOperationsDispatcherExtensions
     {
+        /// <summary>
+        /// Executes an action, guarantees no other dispatches run in parallel with it
+        /// Note - The dispatching is synchronous and doesn't perform the execution, the returned task is to represent the result
+        /// Note - Nested dependent operations will cause deadlocks
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="exec"></param>
+        /// <returns>A task that represents the status of the action execution, can be used to get the results</returns>
         public static Task Dispatch(this ISequentialOperationsDispatcher dispatcher, Action exec)
         {
             return dispatcher.Dispatch(() =>
@@ -14,6 +22,14 @@ namespace Bsii.Dotnet.Utils.Sequential
             });
         }
 
+        /// <summary>
+        /// Executes an action which generates a result value, guarantees no other dispatches run in parallel with it
+        /// Note - The dispatching is synchronous and doesn't perform the execution, the returned task is to represent the result
+        /// Note - Nested dependent operations will cause deadlocks
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="exec"></param>
+        /// <returns>A task that represents the status of the action execution, can be used to get the results</returns>
         public static Task<T> Dispatch<T>(this ISequentialOperationsDispatcher dispatcher, Func<T> exec)
         {
             return dispatcher.Dispatch(() =>
