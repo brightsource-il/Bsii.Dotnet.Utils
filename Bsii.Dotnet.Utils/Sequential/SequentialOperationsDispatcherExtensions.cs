d70de73d8d78f7b719061ae083fcaaf3e1c7eaf5
Gregory Ravikovich <gravikovich@BrightSourceEnergy.com>
2020-05-07 16:17:05 +0300

    Initial basic implementation of sequential operations dispatching, based on patterns used in several places in SFINCS

---
+++ Bsii.Dotnet.Utils/Sequential/SequentialOperationsDispatcherExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Bsii.Dotnet.Utils.Sequential
+{
+    public static class SequentialOperationsDispatcherExtensions
+    {
+        public static Task Dispatch(this ISequentialOperationsDispatcher dispatcher, Action exec)
+        {
+            return dispatcher.Dispatch(() =>
+            {
+                exec();
+                return Task.CompletedTask;
+            });
+        }
+
+        public static Task<T> Dispatch<T>(this ISequentialOperationsDispatcher dispatcher, Func<T> exec)
+        {
+            return dispatcher.Dispatch(() =>
+            {
+                var res = exec();
+                return Task.FromResult(res);
+            });
+        }
+
+    }
+}
