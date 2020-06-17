a1a69641f416ae68b0e8308ebc4c66550a600f91
michaelGershtein <michaelgershtein@gmail.com>
2020-06-17 09:29:47 +0300

    tests wip

--- Bsii.Dotnet.Utils.Tests/DictionaryExtensionsTest.cs
+++ Bsii.Dotnet.Utils.Tests/DictionaryExtensionsTest.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Bsii.Dotnet.Utils.Collections;
 using Xunit;
 
 namespace Bsii.Dotnet.Utils.Tests
 {
+    // TODO: Extensions to test
+    // this Dictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> addValueFactory, Func<TKey, TValue, TValue> updateValueFactory
+    // this Dictionary<TKey, TValue> dictionary, TKey addKey, TValue addValue, TValue updateValue
+    // this Dictionary<TKey, TValue> dictionary, TKey addKey, Func<TKey, TValue> addValueFactory, TValue updateValue
+    // this Dictionary<TKey, TValue> dictionary, TKey key, TValue addValue, Func<TKey, TValue, TValue> updateValueFactory
+    // this Dictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> addValueFactory, Action<TKey, TValue> updateAction
+    // this Dictionary<TKey, TValue> dictionary, TKey addKey, Func<TKey, TValue> addValueFactory, Action updateAction
+    // this Dictionary<TKey, TValue> dictionary, TKey addKey, TValue addValue, Action<TKey, TValue> updateAction
+    // this Dictionary<TKey, TValue> dictionary, TKey addKey, TValue addValue, Action updateAction
+
     public class DictionaryExtensionsTest
     {
         [Fact]
@@ -33,13 +42,13 @@ namespace Bsii.Dotnet.Utils.Tests
             Func<int, int, int> updateValueFactory = (key, value) => 2;
             dict.AddOrUpdate(1, addValueFactory, updateValueFactory);
             Assert.True(dict.ContainsKey(1));
-            Assert.True(dict[1]==1);
+            Assert.True(dict[1] == 1);
         }
 
         [Fact]
         public void TestBasicFactoryUpdate()
         {
-            var dict = new Dictionary<int, int> { [1] = 1 };
+            var dict = new Dictionary<int, int> {[1] = 1};
             Func<int, int> addValueFactory = (key) => 1;
             Func<int, int, int> updateValueFactory = (key, value) => 2;
             dict.AddOrUpdate(1, addValueFactory, updateValueFactory);
