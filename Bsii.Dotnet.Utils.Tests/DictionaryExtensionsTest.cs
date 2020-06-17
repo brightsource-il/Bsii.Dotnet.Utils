f01a1945d71c9e535a6462ae72b29da5560c5849
michaelGershtein <michaelgershtein@gmail.com>
2020-06-17 09:19:18 +0300

    tests wip

--- Bsii.Dotnet.Utils.Tests/DictionaryExtensionsTest.cs
+++ Bsii.Dotnet.Utils.Tests/DictionaryExtensionsTest.cs
@@ -15,5 +15,36 @@ namespace Bsii.Dotnet.Utils.Tests
             dict.AddOrUpdate(1, 1, 1);
             Assert.True(dict.ContainsKey(1));
         }
+
+        [Fact]
+        public void TestBasicUpdate()
+        {
+            var dict = new Dictionary<int, int> {[1] = 1};
+            dict.AddOrUpdate(1, 1, 2);
+            Assert.True(dict.ContainsKey(1));
+            Assert.True(dict[1] == 2);
+        }
+
+        [Fact]
+        public void TestBasicFactoryInsertion()
+        {
+            var dict = new Dictionary<int, int>();
+            Func<int, int> addValueFactory = (key) => 1;
+            Func<int, int, int> updateValueFactory = (key, value) => 2;
+            dict.AddOrUpdate(1, addValueFactory, updateValueFactory);
+            Assert.True(dict.ContainsKey(1));
+            Assert.True(dict[1]==1);
+        }
+
+        [Fact]
+        public void TestBasicFactoryUpdate()
+        {
+            var dict = new Dictionary<int, int> { [1] = 1 };
+            Func<int, int> addValueFactory = (key) => 1;
+            Func<int, int, int> updateValueFactory = (key, value) => 2;
+            dict.AddOrUpdate(1, addValueFactory, updateValueFactory);
+            Assert.True(dict.ContainsKey(1));
+            Assert.True(dict[1] == 2);
+        }
     }
 }
