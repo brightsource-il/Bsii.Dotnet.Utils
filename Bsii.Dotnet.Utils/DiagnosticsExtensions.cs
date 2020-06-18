f695bf73995c9427aff3974d258162e7559137c3
michaelGershtein <michaelgershtein@gmail.com>
2020-06-18 08:50:33 +0300

    minor

--- Bsii.Dotnet.Utils/DiagnosticsExtensions.cs
+++ Bsii.Dotnet.Utils/DiagnosticsExtensions.cs
@@ -5,6 +5,11 @@ namespace Bsii.Dotnet.Utils
 {
     public static class DiagnosticsExtensions
     {
+        /// <summary>
+        /// Starts the activity and returns it as disposable that stops it in case of an error.
+        /// </summary>
+        /// <param name="activity"></param>
+        /// <returns></returns>
         public static IDisposable Use(this Activity activity)
         {
             activity.Start();
