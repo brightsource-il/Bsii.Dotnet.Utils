2d7d4db3a69160a234287858bc2887bbb773c981
Eitan Har-Shoshanim <eitanhs@bseinc.com>
2021-03-17 14:13:18 +0200

    added AsyncEventSource & AsyncValueSource<T>

---
+++ Bsii.Dotnet.Utils/AsyncEventSource.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Bsii.Dotnet.Utils
+{
+    /// <summary>
+    /// Provides synchronization point for multiple awaiters on a single event source
+    /// </summary>
+    public class AsyncEventSource
+    {
+        private readonly AsyncValueSource<bool> _signaler = 
+            new AsyncValueSource<bool>();
+
+        public void Signal() => _signaler.Next(true);
+
+        public Task WaitAsync() => _signaler.GetNextAsync();
+    }
+}
---
+++ Bsii.Dotnet.Utils/AsyncValueSource.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace Bsii.Dotnet.Utils
+{
+    /// <summary>
+    /// Asynchrnously provides <typeparamref name="T"/> values to all awaiting consumers<br/>
+    /// Note: all awaiters will get a reference to the same values (in case that <typeparamref name="T"/> is reference type)
+    /// </summary>
+    public class AsyncValueSource<T>
+    {
+        private TaskCompletionSource<T> _tcs = new TaskCompletionSource<T>(
+            TaskCreationOptions.RunContinuationsAsynchronously);
+
+        public void Next(T value)
+        {
+            var captured = _tcs;
+            _tcs = new TaskCompletionSource<T>(
+                TaskCreationOptions.RunContinuationsAsynchronously);
+            captured.SetResult(value);
+        }
+
+        public Task<T> GetNextAsync()
+        {
+            return _tcs.Task;
+        }
+    }
+}
