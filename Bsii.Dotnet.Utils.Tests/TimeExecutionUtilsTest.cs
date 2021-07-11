f4ead5af63570a9dd32c17157fed84842720b031
Dean Peretz <53830762+dperetz1@users.noreply.github.com>
2021-07-11 22:54:47 +0300

    added async func time execution util (#22)
    * added async func time execution util
    
    * minor
    
    * PR comment + separate util class
    
    * added Task extensions
    
    * minor
    
    * minor
    
    * added unit tests
    
    Co-authored-by: Eitan Har-Shoshanim <52128156+eitanhs@users.noreply.github.com>

---
+++ Bsii.Dotnet.Utils.Tests/TimeExecutionUtilsTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bsii.Dotnet.Utils.Tests
+{
+    public class TimeExecutionUtilsTest
+    {
+        const double TestPrecisionMilliseconds = 20;    // Task.Delay(X) takes a bit more then X ms due to multi threading 
+
+        [Fact]
+        public async Task VoidAsyncFuncUtilTest()
+        {
+            static Task AsyncFunc() => Task.Delay(200);
+            var res  = await TimeExecutionUtils.TimeExecutionAsync(AsyncFunc);
+            AssertResult(TimeSpan.FromMilliseconds(200), res);
+        }
+
+        [Fact]
+        public async Task AsyncFuncUtilTest()
+        {
+            static async Task<int> AsyncFunc()
+            {
+                await Task.Delay(200);
+                return 1;
+            }
+
+            var (res, time)  = await TimeExecutionUtils.TimeExecutionAsync(AsyncFunc);
+            AssertResult(TimeSpan.FromMilliseconds(200), time);
+            Assert.Equal(1, res);
+        }
+
+        [Fact]
+        public async Task VoidTaskUtilTest()
+        {
+            var res  = await Task.Delay(200).TimeExecutionAsync();
+            AssertResult(TimeSpan.FromMilliseconds(200), res);
+        }
+
+        [Fact]
+        public async Task TaskUtilTest()
+        {
+            static async Task<int> AsyncFunc()
+            {
+                await Task.Delay(200);
+                return 1;
+            }
+
+            var (res, time)  = await AsyncFunc().TimeExecutionAsync();
+            AssertResult(TimeSpan.FromMilliseconds(200), time);
+            Assert.Equal(1, res);
+        }
+
+        [Fact]
+        public void VoidSyncFuncUtilTest()
+        {
+            static void Func() => Task.Delay(200).WaitContextless();
+            var res = TimeExecutionUtils.TimeExecution(Func);
+            AssertResult(TimeSpan.FromMilliseconds(200), res);
+        }
+
+        [Fact]
+        public void SyncFuncUtilTest()
+        {
+            static int Func()
+            {
+                Task.Delay(200).WaitContextless();
+                return 1;
+            }
+
+            var (res, time) = TimeExecutionUtils.TimeExecution(Func);
+            AssertResult(TimeSpan.FromMilliseconds(200), time);
+            Assert.Equal(1, res);
+        }
+
+        private static void AssertResult(TimeSpan expected, TimeSpan actual) => 
+            Assert.True(Math.Abs(expected.TotalMilliseconds - actual.TotalMilliseconds) < TestPrecisionMilliseconds);
+    }
+}
---
+++ Bsii.Dotnet.Utils/TimeExecutionUtils.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Bsii.Dotnet.Utils
+{
+    public static class TimeExecutionUtils
+    {
+
+        /// <summary>
+        /// A shorthand for measuring async function  execution time 
+        /// <param name="action">The action to be executed</param>
+        /// <returns> a tuple includes a TimeSpan represents the execution time and the result </returns>
+        /// </summary>
+        public static async Task<(T res, TimeSpan elapsed)> TimeExecutionAsync<T>(this Func<Task<T>> action)
+        {
+            var sw = Stopwatch.StartNew();
+            var res = await action();
+            return (res, sw.Elapsed);
+        }
+
+        /// <summary>
+        /// A shorthand for measuring async function execution time 
+        /// <param name="action">The action to be executed</param>
+        /// <returns> a TimeSpan represents the execution time</returns>
+        /// </summary>
+        public static async Task<TimeSpan> TimeExecutionAsync(this Func<Task> action)
+        {
+            var sw = Stopwatch.StartNew();
+            await action();
+            return sw.Elapsed;
+        }
+
+        /// <summary>
+        /// A shorthand for measuring function execution time 
+        /// <param name="action">The action to be executed</param>
+        /// <returns> a tuple includes a TimeSpan represents the execution time and the result </returns>
+        /// </summary>
+        public static TimeSpan TimeExecution(this Action action)
+        {
+            var sw = Stopwatch.StartNew();
+            action();
+            return sw.Elapsed;
+        }
+
+        /// <summary>
+        /// A shorthand for measuring function execution time 
+        /// <param name="action">The action to be executed</param>
+        /// <returns> a tuple includes a TimeSpan represents the execution time and the result </returns>
+        /// </summary>
+        public static (T res, TimeSpan elapsed) TimeExecution<T>(this Func<T> action)
+        {
+            var sw = Stopwatch.StartNew();
+            var res = action();
+            return (res, sw.Elapsed);
+        }
+
+        /// <summary>
+        /// A shorthand for measuring task execution time 
+        /// <param name="action">The action to be executed</param>
+        /// <returns> a tuple includes a TimeSpan represents the execution time and the result </returns>
+        /// </summary>
+        public static  Task<TimeSpan> TimeExecutionAsync(this Task action) => TimeExecutionAsync(() => action);
+
+        /// <summary>
+        /// A shorthand for measuring task execution time 
+        /// <param name="action">The action to be executed</param>
+        /// <returns> a tuple includes a TimeSpan represents the execution time and the result </returns>
+        /// </summary>
+        public static  Task<(T res, TimeSpan elapsed)> TimeExecutionAsync<T>(this Task<T> action) => TimeExecutionAsync(() => action);
+    }
+}
