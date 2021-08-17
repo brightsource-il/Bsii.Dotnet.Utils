c9cd39b1d0ffaffcc2dde967cf63b39f1ce61c37
eitanhs <eitanhs@brightsourceenergy.com>
2021-08-17 14:17:05 +0300

    implemented AsyncCachingValueSource + AsyncCachingEventSource

---
+++ Bsii.Dotnet.Utils/AsyncCachingEventSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Bsii.Dotnet.Utils
+{
+    public class AsyncCachingEventSource : IAsyncCachingEventProvider
+    {
+        AsyncCachingValueSource<bool> _internalValueSource = new AsyncCachingValueSource<bool>();
+
+        public void Signal() =>
+            _internalValueSource.SetNext(false);
+
+        /// <summary>
+        /// Gets a value - if the last value that was set is younger than the <paramref name="maxAge"/> - returns it,  <br/>
+        /// Otherwise, will wait until a new value was set, or will throw <see cref="TimeoutException"/> if <paramref name="timeOut"/> was set and was exceeded.
+        /// </summary>
+        public async ValueTask WaitAsync(TimeSpan maxAge, TimeSpan? timeOut = default)
+        {
+            await _internalValueSource.GetAsync(maxAge, timeOut);
+        }
+    }
+}
---
+++ Bsii.Dotnet.Utils/IAsyncCachingEventProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Bsii.Dotnet.Utils
+{
+    public interface IAsyncCachingEventProvider
+    {
+        ValueTask WaitAsync(TimeSpan maxAge, TimeSpan? timeOut = null);
+    }
+}
